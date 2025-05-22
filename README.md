d8f1b1a10e90286dbb3223cccb779fd383f6d49f
Manoj Baishya <28330014+manojbaishya@users.noreply.github.com>
2025-05-22 18:40:54 +0530

    Completed all features as described in codingchallenges.fyi/challenges/challenge-grep/

--- README.md
+++ README.md
@@ -5,3 +5,79 @@ https://codingchallenges.fyi/challenges/challenge-grep/
 ## Dependencies:
 
 Picocli - https://picocli.info/
+
+## Usage
+
+```shell
+./grep.sh "<pattern>" "<filename>"
+```
+`<filename>` is from `src/test/resources`.
+or 
+```shell
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep "<pattern>" "<filename>"
+```
+`<filename>` is from `target/test-classes/resources`.
+
+## Features
+
+### Support an empty expression
+
+#### Acceptance Criteria
+**Input Data**:
+```shell
+./grep.sh "" essays/BFS1985.txt | diff target/test-classes/essays/BFS1985.txt -
+./grep.sh "" rockbands.txt | diff target/test-classes/rockbands.txt -
+./grep.sh "" symbols.txt | diff target/test-classes/symbols.txt -
+./grep.sh "" test.txt | diff target/test-classes/test.txt -
+```
+**Output**: Each input must return an empty output.
+
+### Match One-Letter Pattern
+
+```shell
+./grep.sh J rockbands.txt
+```
+**Output**:
+* Judas Priest
+* Bon Jovi
+* Junkyard
+
+### Support recursively listing a directory
+
+```shell
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep --recurse Nirvana ./src/test/resources/
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep --recurse heaven ./src/test/resources/
+```
+
+### Support piping input from standard input
+```shell
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep --recurse Nirvana ./src/test/resources/ | java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep Madonna -
+```
+
+### Support inverse matching
+
+```shell
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep --recursive Nirvana ./src/test/resources/ | java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep -v Springsteen -
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep --recursive Nirvana ./src/test/resources/ | java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep -v Madonna -
+```
+
+### Support digit and word search patterns
+
+```shell
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep "\d" ./src/test/resources/essays/BFS1985.txt
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep "\d" ./src/test/resources/essays/BFS1985.txt
+```
+
+### Support beginning-of-line and end-of-line matching
+
+```shell
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep "^A" ./src/test/resources/rockbands.txt
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep "na$" ./src/test/resources/rockbands.txt
+```
+
+### Support case-insensitive search
+
+```shell
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep A ./src/test/resources/rockbands.txt | wc -l
+java -jar ./target/javagrep-1.0-SNAPSHOT.jar org.dojo.Grep -i A ./src/test/resources/rockbands.txt | wc -l
+```
--- src/main/java/org/dojo/Grep.java
+++ src/main/java/org/dojo/Grep.java
@@ -3,51 +3,103 @@ package org.dojo;
 import picocli.CommandLine;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@CommandLine.Command(name = "Grep", version = "1.0", mixinStandardHelpOptions = true)
+@CommandLine.Command(name = "Grep", version = "1.0.0", mixinStandardHelpOptions = true)
 public class Grep implements Runnable {
-    @CommandLine.Parameters(index = "0")
+    @CommandLine.Parameters(index = "0", hidden = true)
     private String programName;
+
+    @CommandLine.Option(names = {"-i", "--ignore-case"}, description = "Ignore case sensitivity")
+    private boolean ignoreCase;
+
+    @CommandLine.Option(names = {"-r", "--recursive"}, description = "Recurse a directory")
+    private boolean isRecurse;
+
+    @CommandLine.Option(names = {"-v", "--invert-match"}, description = "Invert the search")
+    private boolean isInvertMatch;
 
     @CommandLine.Parameters(index = "1", paramLabel = "<regex>", defaultValue = "", description = "The regex pattern to search in file")
     private String pattern;
 
     @CommandLine.Parameters(index = "2", paramLabel = "<file>", description = "The file to search in")
-    private String filePath;
+    private String path;
 
     @Override
     public void run() {
-        List<String> lines = filterLines(filePath);
-        processData(lines);
+        if (isRecurse) {
+            Path rootPath = Paths.get(path);
+            if (!Files.isDirectory(rootPath)) throw new IllegalArgumentException("Path '%s' is not a directory.".formatted(rootPath.toUri()));
+            List<Path> paths = getAllFilePaths(rootPath);
+            for (Path filepath : paths) {
+                List<String> lines = filterLines(filepath, false);
+                processData(lines, rootPath.toAbsolutePath().relativize(filepath.toAbsolutePath()).toString());
+            }
+        } else {
+            if ("-".equals(path)) {
+                List<String> lines = filterLines(null, true);
+                processData(lines);
+            } else {
+                Path filepath = Paths.get(path);
+                if (!Files.isRegularFile(filepath))
+                    throw new IllegalArgumentException("Path '%s' is not a file. You have to specify the '--recursive' option.".formatted(filepath.toUri()));
+                List<String> lines = filterLines(filepath, false);
+                if (!pattern.isBlank() && lines.isEmpty())
+                    throw new RuntimeException("No lines found for the pattern '%s'".formatted(pattern));
+                processData(lines);
+            }
+        }
     }
 
-    public List<String> filterLines(String filePath) {
+    public List<String> filterLines(Path file, boolean isStandardInput) {
         List<String> lines = new ArrayList<>();
-
-        var file = new File(filePath);
-        if (!file.exists()) throw new IllegalArgumentException("File '%s' not found!".formatted(filePath));
-
-        try (var reader = new BufferedReader(new FileReader(file))) {
+        try (var reader = isStandardInput ? new BufferedReader(new InputStreamReader(System.in)) : Files.newBufferedReader(file)) {
             String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.isBlank()) continue;
+            if (pattern.isBlank()) {
+                while ((line = reader.readLine()) != null) lines.add(line);
+            } else {
+                var regex = ignoreCase ? Pattern.compile(pattern, Pattern.CASE_INSENSITIVE) : Pattern.compile(pattern);
+                if (isInvertMatch) {
+                    while ((line = reader.readLine()) != null) {
+                        Matcher matcher = regex.matcher(line);
+                        if (!matcher.find()) lines.add(line);
+                    }
+                } else {
+                    while ((line = reader.readLine()) != null) {
+                        Matcher matcher = regex.matcher(line);
+                        if (matcher.find()) lines.add(line);
+                    }
+                }
 
-                if (pattern.isBlank()) lines.add(line);
-                else if (line.matches(pattern)) lines.add(line);
             }
         } catch (IOException error) {
             System.err.printf("Error reading file '%s'%n", error.getMessage());
         }
         return lines;
     }
+    private List<Path> getAllFilePaths(Path rootPath) {
+        try(var entries = Files.walk(rootPath)) {
+                return entries.filter(Files::isRegularFile).toList();
+        } catch (IOException error) {
+            System.err.printf("Error reading path '%s'%n", error.getMessage());
+        }
+        return List.of();
+    }
+
+    private void processData(List<String> lines, String filePath) {
+        for (String line : lines) System.out.printf("%s:%s%n", filePath, line);
+    }
 
     private void processData(List<String> lines) {
-        for (var line : lines) System.out.println(line);
+        for (String line : lines) System.out.println(line);
     }
 
     public static void main(String[] args) {
